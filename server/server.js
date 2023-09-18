65b36376d9765cc38112ed1f9eea75aa9c2c1bf2
blaster38847 <blaster38847@gmail.com>
2023-09-17 20:24:49 -0400

    Third Commit

--- server/server.js
+++ server/server.js
@@ -42,10 +42,10 @@ app.use((err,req,res,next)=>{
 })
 
 app.use(express.static('client/build'));
-if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production'){
     const path = require('path');
 
-    app.get('/*', (req,res)=>{
+    app.get('/*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
     })
 }
